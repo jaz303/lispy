2f3923efa3c049247d1dfcd61917f626d09661f7
Jason Frame <jason@onehackoranother.com>
2012-01-01 22:57:27 +0000

    and the header

--- include/lispy/binding.h
+++ include/lispy/binding.h
@@ -7,5 +7,6 @@ int         binding_init(binding_t *binding, binding_t *parent);
 binding_t*	binding_find(binding_t *binding, INTERN key);
 VALUE		binding_get(binding_t *binding, INTERN key);
 void		binding_set(binding_t *binding, INTERN key, VALUE value);
+VALUE       binding_lookup(binding_t *binding, INTERN key);
 
 #endif
