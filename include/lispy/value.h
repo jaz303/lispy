96e4e15cccde00ce6827aa8f9c2c1dfdf842bc07
Jason Frame <jason@onehackoranother.com>
2012-01-09 00:57:10 +0000

    fix bugs in type-tagging and clarify literal values

--- include/lispy/value.h
+++ include/lispy/value.h
@@ -1,23 +1,25 @@
 #ifndef VALUE_H
 #define VALUE_H
 
-#define _INT_BIT				(1 << 0)
+#define _INT_BIT				0x01
 #define _INT_SHIFT				1
 
-#define _SPECIAL_BIT			(1 << 1)
+#define _SPECIAL_BIT			0x02
 #define _SPECIAL_SHIFT			2
 
-#define _SPECIAL_NIL			0
-#define _SPECIAL_FALSE			1
-#define _SPECIAL_TRUE			3
-#define _SPECIAL_ATOM			4
-#define _SPECIAL_IDENT			6
+#define _SPECIAL_NIL			0x00
+#define _SPECIAL_FALSE			0x01
+#define _SPECIAL_TRUE			0x03
+#define _SPECIAL_ATOM			0x04
+#define _SPECIAL_IDENT			0x06
 
-#define _NIL_MASK				(_SPECIAL_BIT | (_SPECIAL_NIL << _SPECIAL_SHIFT))
+#define _SPECIAL_MASK           0x1f
+
+#define _NIL_MASK				(_SPECIAL_BIT | (_SPECIAL_NIL   << _SPECIAL_SHIFT))
 #define _BOOL_MASK				(_SPECIAL_BIT | (_SPECIAL_FALSE << _SPECIAL_SHIFT))
 #define _FALSE_MASK				(_SPECIAL_BIT | (_SPECIAL_FALSE << _SPECIAL_SHIFT))
-#define _TRUE_MASK				(_SPECIAL_BIT | (_SPECIAL_TRUE << _SPECIAL_SHIFT))
-#define _ATOM_MASK				(_SPECIAL_BIT | (_SPECIAL_ATOM << _SPECIAL_SHIFT))
+#define _TRUE_MASK				(_SPECIAL_BIT | (_SPECIAL_TRUE  << _SPECIAL_SHIFT))
+#define _ATOM_MASK				(_SPECIAL_BIT | (_SPECIAL_ATOM  << _SPECIAL_SHIFT))
 #define _IDENT_MASK				(_SPECIAL_BIT | (_SPECIAL_IDENT << _SPECIAL_SHIFT))
 
 #define _IDERN_SHIFT			(_SPECIAL_SHIFT + 3)
@@ -35,8 +37,7 @@
 // Integers
 
 #define VALUE_IS_INT(v)			(((INT)v & _INT_BIT) == _INT_BIT)
-#define MK_INTVAL(i)			((VALUE)(((INT)i << _INT_SHIFT) | _INT_BIT))
-#define SET_INTVAL(v, i)		(v = MK_INTVAL(i))
+#define MK_INTVAL(i)			((VALUE)(((INT)(i) << _INT_SHIFT) | _INT_BIT))
 #define INTVAL(v)				(((INT)v) >> (_INT_SHIFT))
 
 // Nil
@@ -45,7 +46,6 @@
 
 #define VALUE_IS_NIL(v)			(((INT)v) == _NIL_MASK)
 #define MK_NIL()				(_NIL_MASK)
-#define SET_NIL(v)				(v = MK_NIL())
 
 // Boolean
 
@@ -59,20 +59,18 @@
 
 // Atom
 
-#define VALUE_IS_ATOM(v)		(((INT)v & _ATOM_MASK) == _ATOM_MASK)
-#define MK_ATOM(i)				((VALUE)(((INT)i << _IDERN_SHIFT) | _ATOM_MASK))
-#define SET_ATOM(v, i)			(v = MK_ATOM(i))
+#define VALUE_IS_ATOM(v)		(((INT)v & _SPECIAL_MASK) == _ATOM_MASK)
+#define MK_ATOM(i)				((VALUE)(((INT)(i) << _IDERN_SHIFT) | _ATOM_MASK))
 #define ATOM(v)					(((INT)v) >> _IDERN_SHIFT)
 
 // Ident
 
-#define VALUE_IS_IDENT(v)		(((INT)v & _IDENT_MASK) == _IDENT_MASK)
-#define MK_IDENT(i)				((VALUE)(((INT)i << _IDERN_SHIFT) | _IDENT_MASK))
-#define SET_IDENT(v, i)			(v = MK_IDENT(i))
+#define VALUE_IS_IDENT(v)		(((INT)v & _SPECIAL_MASK) == _IDENT_MASK)
+#define MK_IDENT(i)				((VALUE)(((INT)(i) << _IDERN_SHIFT) | _IDENT_MASK))
 #define IDENT(v)				(((INT)v) >> _IDERN_SHIFT)
 
 // Truthy
 
 #define VALUE_IS_TRUTHY(v)		(!VALUE_IS_FALSE(v) && !VALUE_IS_NIL(v))
 
-#endif // VALUE_H
+#endif
